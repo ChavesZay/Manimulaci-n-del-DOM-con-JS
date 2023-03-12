ba3d41c49cc9a8a6824b6901cc05025598945772
Zaylin <chaveszay30@gmail.com>
2023-03-11 21:13:33 -0600

    Clase 04 completada

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+/*Immediately invoked function expression (IIFE)
+(()=>{})() Es para que el usuario no pueda ver el 
+codigo mediante la herramienta de desarrolador*/
+(()=>{
 const btnAgregar= document.querySelector("[data-form-bn]");
 
 //console.log(btnAgregar);
@@ -11,24 +15,42 @@ const createTask=(evento)=>{
     const tarea=document.createElement("li");
     tarea.classList.add("card");
     input.value="";
+    const tareaContent=document.createElement('div');
+    tareaContent.appendChild(checkCompleta());
+
+    const titleTarea= document.createElement('span');
+    titleTarea.classList.add('task');
+    titleTarea.innerText=valor;
+    tareaContent.appendChild(titleTarea);
+
     //backticks  templete String
-    const contenido=`  <div>
-    <i class="far fa-check-square icon"></i>
-    <span class="task">${valor}</span>
-  </div>
-  <i class="fas fa-trash-alt trashIcon icon"></i>`
+    const contenido=`
+   ${checkCompleta()}
+  <i class="fas fa-trash-alt trashIcon icon"></i>`;
  
-  tarea.innerHTML=contenido;
-
+  //tarea.innerHTML=contenido;
+  tarea.appendChild(tareaContent);
   lista.appendChild(tarea);
-    console.log(contenido);
 }
-
-
-
 
 //Arrow functions , funciones de flecha o funciones anonimas
 btnAgregar.addEventListener('click', createTask);
 //listener
 //click
 
+const checkCompleta=()=>{
+    const i= document.createElement("i");
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener('click',completeTarea)
+    return i;
+}
+
+const completeTarea= (event)=>{
+   const element=event.target;
+   //Si existe la clase la quita y si no la pone 
+   element.classList.toggle("fas");
+   element.classList.toggle("completeIcon");
+   element.classList.toggle("far");
+}
+
+})();
